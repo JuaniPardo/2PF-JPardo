c030c02d6c0b21f9a671af1fe68df21df5bb058e
JuaniPardo <juani.pardo@icloud.com>
2024-10-28 16:30:29 -0300

    [RELEASE3]: Listado de cursos en student detail

--- src/app/features/students/student-detail/student-detail.component.ts
+++ src/app/features/students/student-detail/student-detail.component.ts
@@ -1,23 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Student} from "../../../core/models/student";
 import {ActivatedRoute, Router} from "@angular/router";
 import {StudentsService} from "../../../core/services/student.service";
 import {MatDialog} from "@angular/material/dialog";
+
 import {EnrollmentFormComponent} from "../../enrollments/enrollment-form/enrollment-form.component";
+import {EnrollmentService} from "../../../core/services/enrollment.service";
+import {CourseService} from "../../../core/services/course.service";
 
 @Component({
   selector: 'app-student-detail',
   templateUrl: './student-detail.component.html',
   styleUrls: ['./student-detail.component.scss', '../../../shared/styles/details.scss'],
 })
-export class StudentDetailComponent {
+export class StudentDetailComponent implements OnInit {
   studentID!: number;
   student!: Student;
+  enrollments!: EnrollmentDisplay[];
   isLoading: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private studentsService: StudentsService,
+    private enrollmentService: EnrollmentService,
+    private courseService: CourseService,
     private router: Router,
     private dialog: MatDialog
   ) {
@@ -28,6 +34,7 @@ export class StudentDetailComponent {
     this.route.params.subscribe((params) => {
       this.studentID = +params['id']; // uso el '+' para parsear
       this.loadStudent();
+      this.loadEnrollments();
     });
   }
 
@@ -60,4 +67,36 @@ export class StudentDetailComponent {
       }
     });
   }
+
+  private loadEnrollments() {
+    this.enrollmentService.getEnrollmentsByStudentId(this.studentID).subscribe({
+      next: (enrollments) => {
+        this.courseService.getActiveCourses().subscribe({
+          next: (courses) => {
+            this.enrollments = enrollments.map((enrollment) => {
+              const course = courses.find((c) => c.id === enrollment.courseId);
+              return {
+                courseName: course
+                  ? course.name
+                  : 'Curso no encontrado',
+                enrollmentDate: enrollment.enrollmentDate,
+              };
+            });
+          },
+          error: (err) => {
+            console.error(err);
+          },
+        });
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+}
+
+interface EnrollmentDisplay {
+  courseName: string;
+  enrollmentDate: Date;
 }
